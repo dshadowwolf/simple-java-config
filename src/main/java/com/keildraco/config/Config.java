931cd673b2f9e4a9bc2dee12bd1145f83c65a84a
Daniel Hazelton <dshadowwolf@gmail.com>
2018-07-31 03:56:15 -0400

    extend coverage - some changes to Config code to make testing of certain code completely visible and to make testing of that code even possible

--- src/main/java/com/keildraco/config/Config.java
+++ src/main/java/com/keildraco/config/Config.java
@@ -29,7 +29,6 @@ import com.keildraco.config.factory.TypeFactory;
 import com.keildraco.config.types.*;
 import com.keildraco.config.states.*;
 
-import static com.keildraco.config.types.ParserInternalTypeBase.EmptyType;
 import static com.keildraco.config.types.ParserInternalTypeBase.ItemType;
 
 public class Config {
@@ -41,7 +40,9 @@ public class Config {
 			new OperationType(null, "", ""));
 	private static final Map<String, Class<? extends IStateParser>> internalParsers = new ConcurrentHashMap<>();
 	public static final Logger LOGGER = LogManager.getFormatterLogger("config");
-
+	// this is here to make sure we can have an instance for the testing...
+	public static final Config INSTANCE = new Config();
+	
 	static {
 		internalParsers.put("KEYVALUE", KeyValueParser.class);
 		internalParsers.put("LIST", ListParser.class);
@@ -49,36 +50,45 @@ public class Config {
 		internalParsers.put("SECTION", SectionParser.class);
 	}
 
+	private Config() {
+		// do nothing, not even throw
+	}
+	
 	public static TypeFactory getFactory() {
 		return coreTypeFactory;
 	}
 
+	private static IStateParser registerParserGenerator(final String name, final Class<? extends IStateParser> clazz) {
+		Constructor<? extends IStateParser> c;
+		try {
+			c = clazz.getConstructor(TypeFactory.class);
+			return c.newInstance(coreTypeFactory);
+		} catch (final NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			LOGGER.error("Exception getting parser instance for %s: %s", name, e.getMessage());
+			LOGGER.error(e.getStackTrace());
+			return null;
+		}		
+	}
+	
 	private static void registerParserInternal(final String name, final Class<? extends IStateParser> clazz) {
-		coreTypeFactory.registerParser(() -> {
-			Constructor<? extends IStateParser> c;
-			try {
-				c = clazz.getConstructor(TypeFactory.class);
-				return c.newInstance(coreTypeFactory);
-			} catch (final NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				LOGGER.error("Exception getting parser instance for %s: %s", name, e.getMessage());
-				LOGGER.error(e.getStackTrace());
-				return null;
-			}
-		}, name);
+		coreTypeFactory.registerParser(() -> registerParserGenerator(name, clazz), name);
 	}
 
+	private static ParserInternalTypeBase registerTypeGenerator(ParserInternalTypeBase parent, String name, String value,
+			Class<? extends ParserInternalTypeBase> clazz) {
+		Constructor<? extends ParserInternalTypeBase> c;
+		try {
+			c = clazz.getConstructor(ParserInternalTypeBase.class, String.class, String.class);
+			return c.newInstance(parent, name, value);
+		} catch (final NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			LOGGER.error("Exception getting type instance for %s: %s", name, e.getMessage());
+			LOGGER.error(e.getStackTrace());
+			return null;
+		}		
+	}
+	
 	private static void registerTypeInternal(final ItemType type, final Class<? extends ParserInternalTypeBase> clazz) {
-		coreTypeFactory.registerType((parent, name, value) -> {
-			Constructor<? extends ParserInternalTypeBase> c;
-			try {
-				c = clazz.getConstructor(ParserInternalTypeBase.class, String.class, String.class);
-				return c.newInstance(parent==null?EmptyType:parent, name, value);
-			} catch (final NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				LOGGER.error("Exception getting type instance for %s: %s", name, e.getMessage());
-				LOGGER.error(e.getStackTrace());
-				return null;
-			}
-		}, type);
+		coreTypeFactory.registerType((parent, name, value) -> registerTypeGenerator(parent, name, value, clazz), type);
 	}
 
 	public static void registerType(final ItemType type, final Class<? extends ParserInternalTypeBase> clazz) {
@@ -106,8 +116,7 @@ public class Config {
 		tok.slashSlashComments(true);
 		tok.slashStarComments(true);
 		final ParserInternalTypeBase root = coreTypeFactory.getType(null, "root", "", ItemType.SECTION);
-		if(!(root instanceof SectionType)) throw new IllegalStateException("should have gotten a SectionType value back but didn't");
-		else return SectionType.class.cast(coreTypeFactory.getParser("SECTION", root).getState(tok));
+		return SectionType.class.cast(coreTypeFactory.getParser("SECTION", root).getState(tok));
 	}
 
 	private static FileSystem getFilesystemForURI(final URI uri) throws IOException {
--- src/test/java/com/keildraco/config/tests/ConfigAPITest.java
+++ src/test/java/com/keildraco/config/tests/ConfigAPITest.java
@@ -4,15 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import com.keildraco.config.Config;
+import com.keildraco.config.types.ParserInternalTypeBase;
 import com.keildraco.config.types.ParserInternalTypeBase.ItemType;
 
 import static com.keildraco.config.types.ParserInternalTypeBase.EmptyType;
 
 import com.keildraco.config.data.DataQuery;
+import com.keildraco.config.factory.TypeFactory;
+import com.keildraco.config.states.IStateParser;
 import com.keildraco.config.states.SectionParser;
 
 public class ConfigAPITest {
@@ -100,5 +106,101 @@ public class ConfigAPITest {
 		Config.registerKnownParts();
 		final DataQuery c = com.keildraco.config.Config.parseString("section {\n key = [ ident1, ident2, ident3(! ident4)\n}\n\n");
 		assertNotNull(c, String.format("Load worked as expected (%s)", c));
+	}
+
+	@Test
+	public final void testRegisterParserError() {
+		try {
+			Method m = Config.class.getDeclaredMethod("registerParserGenerator", String.class, Class.class);
+			m.setAccessible(true);
+			Object p = m.invoke(Config.INSTANCE, "BROKEN", BrokenParser.class);
+			assertNull(p, "broken parser should return null from generator");			
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			Config.LOGGER.fatal("Exception: %s\nMessage: %s\nStack Dump:", e, e.getMessage());
+			for(StackTraceElement elem : e.getStackTrace()) {
+				Config.LOGGER.fatal("%s", elem);
+			}
+			fail("test broken, exception caught: "+e.getMessage());
+		}
+	}
+	
+	@Test
+	public final void testRegisterTypeError() {
+		try {
+			Method m = Config.class.getDeclaredMethod("registerTypeGenerator", ParserInternalTypeBase.class, String.class, String.class, Class.class);
+			m.setAccessible(true);
+			Object p = m.invoke(Config.INSTANCE, ParserInternalTypeBase.EmptyType, "BROKEN", "BROKEN", BrokenType.class);
+			assertNull(p, "broken type should return null from generator");			
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			Config.LOGGER.fatal("Exception: %s\nMessage: %s\nStack Dump:", e, e.getMessage());
+			for(StackTraceElement elem : e.getStackTrace()) {
+				Config.LOGGER.fatal("%s", elem);
+			}
+			fail("test broken, exception caught: "+e.getMessage());
+		}
+	}
+
+	protected class BrokenType extends ParserInternalTypeBase {
+
+		public BrokenType(ParserInternalTypeBase parent, String name) {
+			super(parent, name);
+		}
+		
+	}
+	protected class BrokenParser implements IStateParser {
+		public BrokenParser(String blargh) {
+			
+		}
+		
+		@SuppressWarnings("unused")
+		private BrokenParser(TypeFactory factory) {
+			
+		}
+		
+		@Override
+		public void setFactory(TypeFactory factory) {
+			// blank, not needed
+		}
+
+		@Override
+		public TypeFactory getFactory() {
+			return null;
+		}
+
+		@Override
+		public void setErrored() {
+			// blank, not needed
+		}
+
+		@Override
+		public boolean errored() {
+			return false;
+		}
+
+		@Override
+		public ParserInternalTypeBase getState(StreamTokenizer tok) {
+			return null;
+		}
+
+		@Override
+		public void setParent(ParserInternalTypeBase parent) {
+			// blank, not needed
+		}
+
+		@Override
+		public ParserInternalTypeBase getParent() {
+			return null;
+		}
+
+		@Override
+		public String getName() {
+			return null;
+		}
+
+		@Override
+		public void clearErrors() {
+			// blank, not needed
+		}
+		
 	}
 }
