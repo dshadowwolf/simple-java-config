3095b2b6f4fb6d60ed0b8fd5ec0f6b03c30bc05f
Daniel Hazelton <dshadowwolf@gmail.com>
2018-08-07 03:05:54 -0400

    add skeleton for missing javadocs

--- src/main/java/com/keildraco/config/interfaces/EmptyParserType.java
+++ src/main/java/com/keildraco/config/interfaces/EmptyParserType.java
@@ -6,35 +6,64 @@ public final class EmptyParserType extends ParserInternalTypeBase {
 
 	private static final String MY_VALUE = "EMPTY";
 
+	/**
+	 *
+	 */
 	public EmptyParserType() {
 		super(null, MY_VALUE, MY_VALUE);
 	}
 
+	/**
+	 *
+	 * @param itemName
+	 * @return
+	 */
 	@Override
 	public boolean has(final String itemName) {
 		return false;
 	}
 
+	/**
+	 *
+	 * @param itemName
+	 * @return
+	 */
 	@Override
 	public ParserInternalTypeBase get(final String itemName) {
 		return Config.EMPTY_TYPE;
 	}
 
+	/**
+	 *
+	 * @param item
+	 */
 	@Override
 	public void addItem(final ParserInternalTypeBase item) {
 		/* the EmptyType does not store other items */
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
 	public ItemType getType() {
 		return ItemType.EMPTY;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
 	public String getValue() {
 		return MY_VALUE;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
 	public String getValueRaw() {
 		return this.getValue();
